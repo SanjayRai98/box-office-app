5b7e0f5349c9d445c1ce8afea00c802779c13174
Sanjay Rai <sanjayharper9852@gmail.com>
2024-01-22 13:59:41 +0530

    adding the api rendering data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
+import { searchForShows } from '../api/tvmaze';
+
 const Home = () => {
+  const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+
+  const onSearchInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    try {
+      const result = await searchForShows(searchStr);
+      setApiData(result);
+    } catch (error) {
+      console.log('error is occurred');
+    }
+  };
+
+  const renderApiData = () => {
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+
+    return null;
+  };
+
   return (
     <div>
-      <h1
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        Home Page
-      </h1>
+      <h1>Home Page</h1>
+      <form onSubmit={onSearch}>
+        <input type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+      <div>{renderApiData()}</div>
     </div>
   );
 };
