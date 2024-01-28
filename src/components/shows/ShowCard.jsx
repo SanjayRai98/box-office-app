3142987ea42406133ab94ccdd22c497ee9e014f0
Sanjay Rai <sanjayharper9852@gmail.com>
2024-01-28 13:51:03 +0530

    adding the Show page for displaying particular page of shows

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const ShowCard = ({ name, image, summary }) => {
+const ShowCard = ({ name, id, image, summary }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(1, 10).join(' ').replace(/<.+?>/g, '')
     : 'No Decription';
@@ -13,7 +13,7 @@ const ShowCard = ({ name, image, summary }) => {
       <h1>{name}</h1>
       <p>{summaryStripped}</p>
       <div>
-        <Link to="/">Read More</Link>
+        <Link to={`/show/${id}`}>Read More</Link>
         <button type="button">Star Me</button>
       </div>
     </div>
