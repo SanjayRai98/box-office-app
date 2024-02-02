a6315793d75d251feab3fefb573d2dce2a62def5
Sanjay Rai <sanjayharper9852@gmail.com>
2024-02-02 10:32:25 +0530

    displayed details data on the show page

---
+++ src/components/shows/ShowMainData.jsx
@@ -0,0 +1,28 @@
+const ShowMainData = ({ image, name, rating, summary, genres }) => {
+  return (
+    <div>
+      <img
+        src={image ? image.original : '/not-found-image.png'}
+        alt={name}
+      ></img>
+
+      <div>
+        <h1>{name}</h1>
+        <div>{rating.average || 'N/A'}</div>
+
+        <div dangerouslySetInnerHTML={{ __html: summary }} />
+
+        <div>
+          Genres:
+          <div>
+            {genres.map(genre => (
+              <span key={genre}>{genres}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowMainData;
