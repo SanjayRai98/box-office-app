c19b4a7a4c13f1bc5868d792a5133192194919eb
Sanjay Rai <sanjayharper9852@gmail.com>
2024-02-11 13:12:34 +0530

    added fade-in animation to FlexGrid. Added scale animation to starBtn

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,11 +1,35 @@
 import styled from 'styled-components';
 import { SearchCard, SearchImgWrapper } from '../../common/SearchCard';
 import { StarIcon } from '../../common/StarIcon';
+import { useRef } from 'react';
 
 const ShowCard = ({ name, id, image, summary, onStarMeClick, isStarred }) => {
   const summaryStripped = summary
     ? summary.split(' ').slice(1, 10).join(' ').replace(/<.+?>/g, '') + ' ...'
     : 'No Decription';
+
+  const starBtnRef = useRef();
+
+  const handleStarClick = () => {
+    onStarMeClick(id);
+
+    const starBtnEl = starBtnRef.current;
+
+    if (!starBtnEl) return;
+
+    if (isStarred) {
+      starBtnEl.classList.remove('animate');
+    } else {
+      starBtnEl.classList.add('animate');
+    }
+
+    hideUnStarShowCard(starBtnEl);
+  };
+
+  const hideUnStarShowCard = starBtnEl => {
+    if (!isStarred) return;
+    starBtnEl.parentElement.parentElement.remove(this);
+  };
 
   return (
     <SearchCard>
@@ -21,7 +45,7 @@ const ShowCard = ({ name, id, image, summary, onStarMeClick, isStarred }) => {
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read More
         </a>
-        <StarBtn type="button" onClick={() => onStarMeClick(id)}>
+        <StarBtn ref={starBtnRef} type="button" onClick={handleStarClick}>
           <StarIcon active={isStarred} />
         </StarBtn>
       </ActionSection>
@@ -57,5 +81,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
